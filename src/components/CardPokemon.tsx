f8bb02e6ea1b00174606e7b6dd4bd8dca659937f
Kevin Molina <kevinmolina.dev@gmail.com>
2024-07-04 00:34:09 -0400

    Fix: Remove unused code and update link in CardPokemon component

--- src/components/CardPokemon.tsx
+++ src/components/CardPokemon.tsx
@@ -1,7 +1,6 @@
-"use client"
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { NextPage } from "next";
+import Link from "next/link";
 
 interface Props {
     id: number,
@@ -10,16 +9,10 @@ interface Props {
 }
 
 export const CardPokemon: NextPage<Props> = ({ id, name, imageUrl }) => {
-    const router = useRouter();
-
-    const handleViewPokemon = () => {
-        router.push(`/pokemons/${id}`);
-    }
-
     return (
-        <div onClick={handleViewPokemon} className="flex flex-col justify-center items-center p-4 border rounded-md border-white hover:cursor-pointer">
+        <Link href={`/pokemons/${id}`} className="flex flex-col justify-center items-center p-4 border rounded-md border-white hover:cursor-pointer">
             <p>{name}</p>
             <Image width={200} height={200} src={imageUrl} alt={name} />
-        </div>
+        </Link>
     );
 };
